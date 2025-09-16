ae8f0e855319b75c0b6d147a701e79401f104451
Samuel Mendes <mendessamuel1@outlook.com>
2025-09-15 22:35:59 -0300

    Format return to entity

---
+++ src/main/java/com/mendes/project_spring/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.mendes.project_spring.services;
+
+import com.mendes.project_spring.entities.Order;
+import com.mendes.project_spring.entities.User;
+import com.mendes.project_spring.repositories.OrderRepository;
+import com.mendes.project_spring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    };
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    };
+}
