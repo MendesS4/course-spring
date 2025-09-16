edbccb90f6dcb43d9618cf612017065a12664d07
Samuel Mendes <mendessamuel1@outlook.com>
2025-09-15 22:53:47 -0300

    New enum OrderStatus

--- src/main/java/com/mendes/project_spring/config/TestConfig.java
+++ src/main/java/com/mendes/project_spring/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.mendes.project_spring.config;
 
 import com.mendes.project_spring.entities.Order;
 import com.mendes.project_spring.entities.User;
+import com.mendes.project_spring.entities.enums.OrderStatus;
 import com.mendes.project_spring.repositories.OrderRepository;
 import com.mendes.project_spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,10 +30,10 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         User u3 = new User(null, "Samuel", "samuel@gmail.com", "7776666228", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2025-08-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2025-09-08T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2025-09-10T15:21:22Z"), u1);
-        Order o4 = new Order(null, Instant.parse("2025-09-11T15:21:22Z"), u3);
+        Order o1 = new Order(null, Instant.parse("2025-08-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2025-09-08T03:42:10Z"), OrderStatus.CANCELED, u2);
+        Order o3 = new Order(null, Instant.parse("2025-09-10T15:21:22Z"), OrderStatus.SHIPPED,u1);
+        Order o4 = new Order(null, Instant.parse("2025-09-11T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u3);
 
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
