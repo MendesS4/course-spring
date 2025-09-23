b00c1e97a84238641704301f60e61fe7c29f5c2b
Samuel Mendes <mendessamuel1@outlook.com>
2025-09-22 21:40:15 -0300

    Exception handling - findById

---
+++ src/main/java/com/mendes/project_spring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.mendes.project_spring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found with id " + id);
+    }
+
+}
+
