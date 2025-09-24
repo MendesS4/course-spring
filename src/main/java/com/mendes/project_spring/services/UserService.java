274b5059c51526eae3255157686979c85f0171c6
Samuel Mendes <mendessamuel1@outlook.com>
2025-09-23 22:05:38 -0300

    Exception update user

--- src/main/java/com/mendes/project_spring/services/UserService.java
+++ src/main/java/com/mendes/project_spring/services/UserService.java
@@ -4,6 +4,7 @@ import com.mendes.project_spring.entities.User;
 import com.mendes.project_spring.repositories.UserRepository;
 import com.mendes.project_spring.services.exceptions.DatabaseException;
 import com.mendes.project_spring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     private void updateData(User entity, User obj) {
